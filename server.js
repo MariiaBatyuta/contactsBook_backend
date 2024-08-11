8055910c336b195f9bab5b50161e4d07e67976da
MariiaBatyuta <mashabatyuta.work@gmail.com>
2024-08-11 13:33:24 +0200

    adde swagger documentation

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ import "./db/db.js";
 
 import express from "express";
 import cors from "cors";
+import fs from "fs";
+import path from "path";
+import swaggerUi from "swagger-ui-express";
 
 import authRouter from "./routes/authRoutes.js";
 import contactsRouter from "./routes/contactsRoutes.js";
 
 const app = express();
+const swaggerDocumentation = JSON.parse(fs.readFileSync(path.resolve("./swagger.json"), 'utf-8'));
 
 app.use(cors());
 
@@ -15,6 +19,16 @@ app.use(express.json());
 
 app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocumentation));
+
+app.get("api/warm-up", (req, res, next) => {
+    try {
+        res.status(200).send({message: "Server is warmed up. You can use the web."})
+    } catch (error) {
+        next(error);
+    }
+});
 
 app.use((_, res) => {
     res.status(400).send({ message: "Route not found" });
